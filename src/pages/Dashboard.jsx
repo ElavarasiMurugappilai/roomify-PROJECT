51cd27c98b29e4e2fd140b55dbf1e6d67082c4e5
Elavarasi <124075894+ElavarasiMurugappilai@users.noreply.github.com>
2025-05-20 16:19:44 +0530

    fix: Correctly wrap dashboard title in center tags

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,7 +6,9 @@ import { Container, Typography } from '@mui/material';
 const Dashboard = () => {
   return (
     <Container>
+      <center>
       <Typography variant="h4">Welcome to Roomify Dashboard</Typography>
+      </center>
     </Container>
   )
 }
